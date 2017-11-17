393b6c5e314375f07e8db8787ef8271209842ff8
maryfranks <maryfranks@gmail.com>
2017-11-17 17:25:44 -0500

    add barebones express server

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.listen(3000);
+
+console.log("Express app running on port 3000...");
+
+module.exports = app;
