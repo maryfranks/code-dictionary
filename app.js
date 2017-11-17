f7c634e8d1aa9f251a55b18e30c1dac3754fee5a
maryfranks <maryfranks@gmail.com>
2017-11-17 17:45:28 -0500

    add some data

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
 
+var codeTerms = [
+  {
+    term: "closure",
+    defined: "A function within a function that can be stored as a variable"
+  },
+  {
+    term: "==",
+    defined: "Equality with coersion"
+  },
+  {
+    term: "callback",
+    defined: "Executable code passed as an argument to another piece of code"
+  },
+];
+
 app.use(function(request, response, next) {
 
   console.log(`${request.method} request for ${request.url}`);
   next();
-  
+
 });
 
 app.use(express.static("./public"));
